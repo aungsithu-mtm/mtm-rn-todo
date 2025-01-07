c0d5487184907f30bbe2f3212ab3f907332d1365
aungsithu-mtm <aungsithu@metateammyanmar.com>
2025-01-07 17:06:36 +0630

    initial commit

---
+++ apollo/client.ts
@@ -0,0 +1,36 @@
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
+const httpLink = new HttpLink({
+    uri: process.env.EXPO_PUBLIC_APOLLO_ENDPOINT,
+});
+
+const authLink = setContext(async (_, { headers }) => {
+    try {
+        const token = await AsyncStorage.getItem("token");
+        return {
+            headers: {
+                ...headers,
+                authorization: token ? `Bearer ${token}` : "",
+            },
+        };
+    } catch (error) {
+        console.log("Error Retreving Token:", error);
+        return {
+            headers: {
+                ...headers,
+            }
+        };
+    }
+});
+
+const apolloClient = () => {
+    const client = new ApolloClient({
+        cache: new InMemoryCache(),
+        link: authLink.concat(httpLink),
+    });
+    return client
+}
+
+export default apolloClient;
---
+++ constants/Color.ts
@@ -0,0 +1,20 @@
+export const Colors = {
+    light: {
+        primary: '#EB8317',
+        secondary: '#F3C623',
+        third: '#10375C',
+        grey: '#A6A3AF',
+        white: '#F4F6FF',
+        black: '#292929',
+        danger: '#FF4C4C'
+    },
+    dark: {
+        primary: '#FFB870',
+        secondary: '#FFC857',
+        third: '#1A2E45',
+        grey: '#6C6C77',
+        white: '#1E1E1E',
+        black: '#F4F6FF',
+        danger: '#FF6B6B',
+    },
+};
